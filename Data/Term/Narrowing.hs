cbce9d018148790408bbad9f72d276d442db3c2e
pepe iborra <pepeiborra@gmail.com>
2015-11-07 10:54:46 +0000

    Change qNarrow argument from an extensive to an intensive set

--- Data/Term/Narrowing.hs
+++ Data/Term/Narrowing.hs
@@ -166,18 +166,18 @@ narrows' rr = liftM (second zonkSubst) . run(closureMP(narrowStepBasic rr >=> zo
 -- ---------------------------
 -- | Note that this function does not assume that the rules and the term have been renamed apart
 qNarrow1P :: ( Ord v, Observable v, Enum v, Rename v, Unify t, MonadPlus m, Observable1 t
-             ) => [Term t v] -> [Rule t v] -> Term t v -> m ((Term t v, Position), Substitution t v)
+             ) => (Term t v -> Bool) -> [Rule t v] -> Term t v -> m ((Term t v, Position), Substitution t v)
 qNarrow1P q rr t = second(restrictTo (vars t)) `liftM` qNarrow1P' q rr t
 -- | Note that this function does not assume that the rules and the term have been renamed apart
 qNarrow1P' :: ( Ord v, Observable v, Enum v, Rename v, Unify t, MonadPlus m, Observable1 t
-              ) => [Term t v] -> [Rule t v] -> Term t v -> m ((Term t v, Position), Substitution t v)
+              ) => (Term t v -> Bool) -> [Rule t v] -> Term t v -> m ((Term t v, Position), Substitution t v)
 qNarrow1P' q rr = liftM(second zonkSubst) . run (qNarrowStepBasic q rr >=> firstM(zonkM return))
 
 {-# INLINE qNarrowStepBasic #-}
 -- Note that this function does not assume that the rules and the term have been renamed apart
 qNarrowStepBasic :: (Unify t, Enum v, Ord v, Observable v, Observable1 t, MonadPlus m, MonadVariant m, MonadEnv m, Var m ~ v, t ~ TermF m) =>
-                   [Term t v] -> [Rule t v] -> Term t v -> m (Term t v, Position)
-qNarrowStepBasic q rr t = go (t, mempty, [])
+                   (Term t v -> Bool) -> [Rule t v] -> Term t v -> m (Term t v, Position)
+qNarrowStepBasic isQNF rr t = go (t, mempty, [])
     where go (t, ct,pos) = do { t' <- narrowTop t;
                                 return (ct |> t', pos)}
                           `mplus`
@@ -189,7 +189,6 @@ qNarrowStepBasic q rr t = go (t, mempty, [])
                           lhs' <- zonkTermM return lhs
                           forM_ (directSubterms lhs') (guard . isQNF)
                           return rhs
-          isQNF = isNF [ qt :-> qt | qt <- q]
 
 #ifdef LOGICT
 -- | Innermost narrowing
